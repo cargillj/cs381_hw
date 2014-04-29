49a4122708a96ff1f6c7f0a05de2f3494c553d98
John Cargill <cargillj@onid.orst.edu>
2014-04-28 20:25:06 -0700

    added hw2, havn't worked yet

---
+++ hw2/Hw2.hs
@@ -0,0 +1,14 @@
+--	HW2: Semantics
+--	CS 381 Spring 2014
+--	Authors: Aaron Egger, John Cargill
+
+module Hw2 where
+
+--Exercise 1. A Stack Language
+
+--Exercise 2. Extending the Stack Language by Macros
+--(a)
+--(b)
+--(c)
+
+--Exercise 3. Mini Logo
