a7850739349e69f8948becee547b5cfd7cb478d7
John Cargill <cargillj@onid.orst.edu>
2014-04-28 21:59:17 -0700

    Put some of the code from the description in

--- hw2/Hw2.hs
+++ hw2/Hw2.hs
@@ -6,6 +6,19 @@ module Hw2 where
 
 --Exercise 1. A Stack Language
 
+type Prog = [Cmd]
+
+data Cmd = LD Int
+		| ADD
+		| MULT
+		| DUP
+
+type Stack = [Int]
+
+sem :: Prog -> D
+
+semCmd :: Cmd -> D
+
 --Exercise 2. Extending the Stack Language by Macros
 --(a)
 --(b)
