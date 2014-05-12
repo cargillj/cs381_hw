69e6a2a8b4a22a320480cdadbdb8d1585f484f14
cargillj <cargillj@onid.oregonstate.edu>
2014-05-12 16:03:01 -0700

    did some work on ex1

--- hw3/Hw3.hs
+++ hw3/Hw3.hs
@@ -5,4 +5,54 @@
 module Hw3 where
 
 --Exercise 1. A Rank-Based Type Systems for the Stack Language
+--(a)
+type prog = [Cmd]
 
+type Stack = [Int]
+
+type D = Maybe -> Maybe
+
+sem :: Prog -> D
+sem []		c = c
+sem (o:os)	c = sem os (semCmd o c)
+
+semCmd :: Cmd -> D 
+semCmd (LD i)   xs = (i:xs) 
+semCmd ADD      xs = ((last xs) + ((last . init) xs)):(drop 2 xs) 
+semCmd MULT     xs = ((last xs) * ((last . init) xs)):(drop 2 xs)
+semCmd DUP      xs = (last xs):xs
+semCmd INC      xs = ((last xs) + 1) 
+semCmd SWAP     xs = ((last . init xs):(last xs)):(drop 2 xs)
+semCmd (POP i)  xs = (drop i xs)  
+semCmd _	Error
+--errorcase|nothing
+
+
+data Cmd = LD Int
+	 | ADD
+	 | MULT
+	 | DUP
+	 | INC
+	 | SWAP
+	 | POP Int
+	deriving Show
+
+type Rank = Int
+type CmdRank = (Int,Int)
+
+rankC :: Cmd -> CmdRank
+rankC (LD i)  = (0, 1)
+rankC ADD     = (2, 1)
+rankC MULT    = (2, 1)
+rankC DUP     = (1, 2)
+rankC INC     = (1, 1)
+rankC SWAP    = (2, 2)
+rankC (POP i) = (i, 0) 
+
+
+rank :: Prog -> Rank -> Maybe Rank
+rank [] r | r >= 0 = just r 
+rank (x:xs) r | currenttotal >= 
+
+rankP :: Prog -> Maybe Rank
+rankP xs = rank xs 0 
