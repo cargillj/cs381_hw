688eaeb6f16a5e145a0ec91b9a3e2bf6810d94ea
John Cargill <cargillj@onid.orst.edu>
2014-05-12 22:23:47 -0700

    finished Ex.2

--- hw3/Hw3.hs
+++ hw3/Hw3.hs
@@ -67,3 +67,42 @@ semStatTc p | typeSafe p = Just (sem p ([]))
 p1 = [LD 3, DUP, ADD, LD 5, SWAP]
 
 --Exercise 2. Shape Language
+data Shape = X
+		   | TD Shape Shape
+		   | LR Shape Shape
+		   deriving Show
+
+type BBox = (Int,Int)
+
+--(a)
+bbox :: Shape -> BBox
+bbox X = (1,1)
+bbox (TD s1 s2)
+	| s1x >= s2x	= (s1x, s1y + s2y)
+	| s1x < s2x		= (s2x, s1y + s2y)
+	where	
+		(s1x, s1y) = bbox s1 
+		(s2x, s2y) = bbox s2
+
+bbox (LR s1 s2)
+	| s1y >= s2x	= (s1x + s2x, s1y)
+	| s1y < s2x		= (s1x + s2x, s2y)
+	where	
+		(s1x, s1y) = bbox s1
+		(s2x, s2y) = bbox s2
+
+--(b)
+rect :: Shape -> Maybe BBox
+rect X = Just (1,1)
+rect (TD s1 s2)
+	| Just s1x == Just s2x = Just (s1x, s1y + s2y)
+	| otherwise = Nothing
+	where	
+		Just (s1x, s1y) = rect s1
+		Just (s2x, s2y) = rect s2
+rect (LR s1 s2)
+	| Just s1y == Just s2y = Just (s1x + s2x, s1y)
+	| otherwise = Nothing
+	where	
+		Just (s1x, s1y) = rect s1
+		Just (s2x, s2y) = rect s2
